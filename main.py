35a014426c749749d98b6d2585c35581fdff0797
grishhung <justinhung@gmail.com>
2023-02-24 18:20:26 -0500

    refactored main

--- main.py
+++ main.py
@@ -10,36 +10,52 @@ def isfloat(num: typing.Any) -> bool:
         return False
 
 
-class Time:
-    # TO DO: COMPARTMENTALIZE THIS CLASS!!
-    time_create: float
-    length: float
-    name: str
+class CPUBurst:
+    def __init__(self, ats: float, ate: float, cts: float, cte: float) -> None:
+        self.arrv_t_start = ats
+        self.arrv_t_end: ate
+        self.comp_t_start: cts
+        self.comp_t_end: cte
 
-    def __init__(self, tc: float, l: float; n: str) -> None:
-        self.time_create = tc
-        length = l
-        name = n
+
+class Process:
+    def __init__(self, p: str, s: str) -> None:
+        self.pid = p
+        self.state = s
+
+
+class CPU:
+    def __init__(self, n: int, ncpu: int, s: float, l: float, ub: int) -> None:
+        self.num_proc = n
+        self.num_proc_cpu = ncpu
+        self.seed_val = s
+        self.lambda_val = l
+        self.up_bound = ub
+
+    def debug_print(self):
+        print("n: {}, ncpu: {}, seed: {}, lam: {}, upperBound: {}"
+            .format(self.num_proc, self.num_proc_cpu, self.seed_val,
+                    self.lambda_val, self.up_bound))
 
 
 def main():
-    if (len(sys.argv) != 6)
+    if ((len(sys.argv) != 6)
             or (not sys.argv[1].isdigit())
             or (not sys.argv[2].isdigit())
             or (not isfloat(sys.argv[3]))
             or (not isfloat(sys.argv[4]))
-            or (not sys.argv[2].isdigit()):
-        # some sort of error handling here
-        return -1
+            or (not sys.argv[2].isdigit())):
+        print("ERROR!");
+        return
 
-    n = int(sys.argv[1])           # number of processes to simulate 
-    ncpu = int(sys.argv[2])        # number of CPU bound processes
-    seed = float(sys.argv[3])      # seed value for random number seq.
-    lam = float(sys.argv[4])       # exp. distribution lambda value
-    upperBound = int(sys.argv[5])  # upper bound for valid random nums
+    n = int(sys.argv[1])            # number of processes to simulate 
+    ncpu = int(sys.argv[2])         # number of CPU bound processes
+    seed = float(sys.argv[3])       # seed value for random number seq.
+    lam = float(sys.argv[4])        # exp. distribution lambda value
+    upper_bound = int(sys.argv[5])  # upper bound for valid random nums
 
-    print("n: {}, ncpu: {}, seed: {}, lam: {}, upperBound: {}"
-            .format(n, ncpu, seed, lam, upperBound))
+    cpu = CPU(n, ncpu, seed, lam, upper_bound)
+    cpu.debug_print()
 
 
 if __name__ == "__main__":
